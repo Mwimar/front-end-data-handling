26c212182d01f59d2e9d68c25a6236ac8f810641
Mark Mwima <markmwimar@gmail.com>
2023-10-08 22:02:34 +0300

    serving html files

---
+++ app.js
@@ -0,0 +1,34 @@
+const express=require('express');
+const app = express();
+app.use(express.urlencoded({extended:false}))
+const fs=require('fs');
+const path = require('path');
+
+app.get('/', function (req, res) {
+    res.send('<h1>Whats Good Niggas</h1>')
+})
+
+
+app.get('/restaurants', function (req, res) {
+    const htmlFilePath = path.join(__dirname, 'views', 'restaurants.html');
+    res.sendFile(htmlFilePath)
+})
+
+app.get('/index', function (req, res) {
+    const htmlFilePath = path.join(__dirname, 'views', 'index.html');
+    res.sendFile(htmlFilePath);
+})
+
+app.get('/recommend', function (req, res) {
+    const htmlFilePath = path.join(__dirname, 'views', 'recommend.html');
+    res.sendFile(htmlFilePath);
+})
+
+app.get('/about', function (req, res) {
+    const htmlFilePath = path.join(__dirname, 'views', 'about.html');
+    res.sendFile(htmlFilePath);
+})
+
+
+
+app.listen(3000);
